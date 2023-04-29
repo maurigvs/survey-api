4529f8b1fbb38a75be3cc25eca844ed87f1ec3a1
Mauri Gonçalves <maurigvs@icloud.com>
2023-04-29 12:56:31 -0300

    first commit

---
+++ src/main/java/br/com/maurigvs/surveyapi/SurveyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.maurigvs.surveyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maurigvs/surveyapi/SurveyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maurigvs.surveyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
