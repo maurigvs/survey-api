04a494d9b685fe68ccf6e9f46677f6fbf0edfe74
Mauri Gonçalves <maurigvs@icloud.com>
2023-04-29 14:02:21 -0300

    sonar fixes

--- src/test/java/br/com/maurigvs/surveyapi/SurveyApiApplicationTests.java
+++ src/test/java/br/com/maurigvs/surveyapi/SurveyApiApplicationTests.java
@@ -1,13 +1,21 @@
 package br.com.maurigvs.surveyapi;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.maurigvs.surveyapi.controller.PropertiesController;
 
 @SpringBootTest
 class SurveyApiApplicationTests {
 
+	@Autowired
+	private PropertiesController propertiesController;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(propertiesController);
 	}
-
 }
